e62ab3db26165a97e389e7306fc877e4eaf9e20a
Evtushenko Georgy <evtushenko.georgy@gmail.com>
2021-08-16 16:31:10 +0300

    Document file

--- cub/device/device_segmented_sort.cuh
+++ cub/device/device_segmented_sort.cuh
@@ -25,6 +25,13 @@
  *
  ******************************************************************************/
 
+/**
+ * \file
+ * cub::DeviceSegmentedSort provides device-wide, parallel operations for
+ * computing a batched sort across multiple, non-overlapping sequences of
+ * data items residing within device-accessible memory.
+ */
+
 #pragma once
 
 #include "../config.cuh"
