3e6779c73d1eaceab6cf25332ed159d89d307f28
Georgy Evtushenko <evtushenko.georgy@gmail.com>
2021-08-21 13:40:34 +0300

    Type test

--- test/gtest_block_adjacent_difference.cu
+++ test/gtest_block_adjacent_difference.cu
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+// Value parameter
 enum class MyType
 {
   MY_FOO = 0,
@@ -12,10 +13,45 @@ class MyTestSuite
 
 INSTANTIATE_TEST_SUITE_P(
   MyGroup,
+
   MyTestSuite,
+
   testing::Combine(testing::Values(MyType::MY_FOO, MyType::MY_BAR),
                    testing::Values("A", "B")),
+
   [](const testing::TestParamInfo<MyTestSuite::ParamType> &info) {
     std::string name = std::get<0>(info.param) == MyType::MY_FOO ? "Foo" : "Bar";
     return name;
   });
+
+
+// Type parameter
+struct A1 {
+  char ch = 'A';
+};
+
+struct A2 {
+  char ch = 'a';
+};
+
+struct B1 {
+  char ch = 'B';
+};
+
+struct B2 {
+  char ch = 'b';
+};
+
+
+template <typename T>
+class pair_test : public ::testing::Test {};
+
+using test_types = ::testing::Types<std::pair<A1,A2>, std::pair<B1,B2>>;
+TYPED_TEST_SUITE(pair_test, test_types);
+
+TYPED_TEST(pair_test, compare_no_case)
+{
+  typename TypeParam::first_type param1;
+  typename TypeParam::second_type param2;
+  ASSERT_TRUE(param1.ch == std::toupper(param2.ch));
+}
