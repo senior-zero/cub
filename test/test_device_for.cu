56032d85e3490385b7c94ac5d2053e226ed623e4
Georgy Evtushenko <evtushenko.georgy@gmail.com>
2022-04-13 13:35:42 +0400

    Continue thinking about vectorization

--- test/test_device_for.cu
+++ test/test_device_for.cu
@@ -46,6 +46,19 @@ struct Marker
   }
 };
 
+struct Counter
+{
+  int *d_count{};
+
+  __device__ void operator()(int i) const
+  {
+    if (i == 42)
+    {
+      atomicAdd(d_count, 1);
+    }
+  }
+};
+
 struct Checker
 {
   int *d_marks{};
@@ -68,21 +81,68 @@ int main(int argc, char** argv)
   // Initialize device
   CubDebugExit(args.DeviceInit());
 
-  const int n = 1024 * 1024;
-  thrust::device_vector<int> marks(n);
-  int *d_marks = thrust::raw_pointer_cast(marks.data());
-  Marker op{d_marks};
+  const int n = 32 * 1024 * 1024;
 
-  auto striped_tuning = cub::TuneForEach<cub::ForEachAlgorithm::BLOCK_STRIPED>(
-    cub::ForEachConfigurationSpace{}.Add<1024, 4>()
-                                    .Add<256, 4>());
+  // Test
+  {
+    thrust::device_vector<int> marks(n);
+    int *d_marks = thrust::raw_pointer_cast(marks.data());
+    Marker op{d_marks};
 
-  auto vectorized_tuning = cub::TuneForEach<cub::ForEachAlgorithm::VECTORIZED>(
-    cub::ForEachConfigurationSpace{}.Add<1024, 4>()
-                                    .Add<256, 4>());
+    auto tuning = cub::TuneForEach<cub::ForEachAlgorithm::BLOCK_STRIPED>(
+      cub::ForEachConfigurationSpace{}.Add<1024, 4>()
+        .Add<256, 4>());
 
-  cub::DeviceFor::Bulk(n, op, {}, true, striped_tuning);
-  cub::DeviceFor::ForEachN(d_marks, n, op, {}, true, vectorized_tuning);
+    cub::DeviceFor::Bulk(n, op, {}, true, tuning);
+    AssertEquals(n, thrust::count(marks.begin(), marks.end(), 1));
+  }
 
-  AssertEquals(n, thrust::count(marks.begin(), marks.end(), 1));
+  // Bench
+  {
+    thrust::device_vector<int> in_1(n, 33);
+    thrust::device_vector<int> in_2(n, 32);
+
+    thrust::device_vector<int> counter_1(n);
+    thrust::device_vector<int> counter_2(n);
+
+    int *d_in_1 = thrust::raw_pointer_cast(in_1.data());
+    int *d_in_2 = thrust::raw_pointer_cast(in_2.data());
+
+    int *d_counter_1 = thrust::raw_pointer_cast(counter_1.data());
+    int *d_counter_2 = thrust::raw_pointer_cast(counter_2.data());
+
+    Counter op_1{d_counter_1};
+    Counter op_2{d_counter_2};
+
+    auto striped_tuning = cub::TuneForEach<cub::ForEachAlgorithm::BLOCK_STRIPED>(
+      cub::ForEachConfigurationSpace{}.Add<1024, 1>()
+                                      .Add<256, 1>());
+
+    auto vectorized_tuning = cub::TuneForEach<cub::ForEachAlgorithm::VECTORIZED>(
+      cub::ForEachConfigurationSpace{}.Add<1024, 1>()
+                                      .Add<256, 1>());
+
+    float striped_ms, vectorized_ms;
+    cudaEvent_t begin, end;
+
+    cudaEventCreate(&begin);
+    cudaEventCreate(&end);
+
+    cudaEventRecord(begin);
+    cub::DeviceFor::ForEachN(d_in_1, n, op_1, {}, true, striped_tuning);
+    cudaEventRecord(end);
+    cudaEventSynchronize(end);
+    cudaEventElapsedTime(&striped_ms, begin, end);
+
+    cudaEventRecord(begin);
+    cub::DeviceFor::ForEachN(d_in_2, n, op_2, {}, true, vectorized_tuning);
+    cudaEventRecord(end);
+    cudaEventSynchronize(end);
+    cudaEventElapsedTime(&vectorized_ms, begin, end);
+
+    cudaEventDestroy(begin);
+    cudaEventDestroy(end);
+
+    std::cout << striped_ms << " / " << vectorized_ms << " = " << striped_ms / vectorized_ms << std::endl;
+  }
 }
